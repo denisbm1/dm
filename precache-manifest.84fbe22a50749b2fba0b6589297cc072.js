8125f33ecee4d853f268b465e08e0afb0b3ee779
denisbm1 <denisbm@mail.ru>
2020-05-28 11:51:14 +0300

    Updates

---
+++ precache-manifest.84fbe22a50749b2fba0b6589297cc072.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "00b7075650f6c20b731a",
+    "url": "/dm/static/css/main.0955bfa8.chunk.css"
+  },
+  {
+    "revision": "00b7075650f6c20b731a",
+    "url": "/dm/static/js/main.00b70756.chunk.js"
+  },
+  {
+    "revision": "da19d43c29c1ce85e6b4",
+    "url": "/dm/static/css/1.c43d9e20.chunk.css"
+  },
+  {
+    "revision": "da19d43c29c1ce85e6b4",
+    "url": "/dm/static/js/1.da19d43c.chunk.js"
+  },
+  {
+    "revision": "e95ec42567a2cb74c1ef",
+    "url": "/dm/static/css/2.9e7b6785.chunk.css"
+  },
+  {
+    "revision": "e95ec42567a2cb74c1ef",
+    "url": "/dm/static/js/2.e95ec425.chunk.js"
+  },
+  {
+    "revision": "d2d909de6513f0cbcf23",
+    "url": "/dm/static/css/3.48999753.chunk.css"
+  },
+  {
+    "revision": "d2d909de6513f0cbcf23",
+    "url": "/dm/static/js/3.d2d909de.chunk.js"
+  },
+  {
+    "revision": "0d0a065e5edc76c3db63",
+    "url": "/dm/static/js/4.0d0a065e.chunk.js"
+  },
+  {
+    "revision": "6d5a3b1e247ff7bab766",
+    "url": "/dm/static/js/runtime~main.6d5a3b1e.js"
+  },
+  {
+    "revision": "95c92a3c05ab58817c907cdc209d6941",
+    "url": "/dm/static/media/orig.95c92a3c.gif"
+  },
+  {
+    "revision": "9600ea585734437bfeed9f0edd442a17",
+    "url": "/dm/index.html"
+  }
+];
