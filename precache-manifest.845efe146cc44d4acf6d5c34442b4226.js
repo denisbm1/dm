cb29cfee2d9cd692757392824797f29b4f005dc2
denisbm1 <denisbm@mail.ru>
2020-05-29 10:33:58 +0300

    Updates

---
+++ precache-manifest.845efe146cc44d4acf6d5c34442b4226.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fb2bf1dfa7ece610d11",
+    "url": "/dm/static/css/main.e461c026.chunk.css"
+  },
+  {
+    "revision": "3fb2bf1dfa7ece610d11",
+    "url": "/dm/static/js/main.3fb2bf1d.chunk.js"
+  },
+  {
+    "revision": "445c16b72ad779a5b764",
+    "url": "/dm/static/css/1.0019717c.chunk.css"
+  },
+  {
+    "revision": "445c16b72ad779a5b764",
+    "url": "/dm/static/js/1.445c16b7.chunk.js"
+  },
+  {
+    "revision": "825d8ee24e793e100df8",
+    "url": "/dm/static/css/2.a3e3bfa6.chunk.css"
+  },
+  {
+    "revision": "825d8ee24e793e100df8",
+    "url": "/dm/static/js/2.825d8ee2.chunk.js"
+  },
+  {
+    "revision": "bc4348748ab123f86ca0",
+    "url": "/dm/static/css/3.c434a181.chunk.css"
+  },
+  {
+    "revision": "bc4348748ab123f86ca0",
+    "url": "/dm/static/js/3.bc434874.chunk.js"
+  },
+  {
+    "revision": "cb089209951817a26d15",
+    "url": "/dm/static/js/4.cb089209.chunk.js"
+  },
+  {
+    "revision": "95a7bb780e338c308782",
+    "url": "/dm/static/js/runtime~main.95a7bb78.js"
+  },
+  {
+    "revision": "6904edef0777ce8b7bcad3a880743cfa",
+    "url": "/dm/static/media/ava.6904edef.jpg"
+  },
+  {
+    "revision": "95c92a3c05ab58817c907cdc209d6941",
+    "url": "/dm/static/media/orig.95c92a3c.gif"
+  },
+  {
+    "revision": "4ef37ee5ff49178b844be6bb30fe9a1d",
+    "url": "/dm/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dm/precache-manifest.a0ffe3f0ea11d6ee0d537769e69597af.js"
+  "/dm/precache-manifest.845efe146cc44d4acf6d5c34442b4226.js"
 );
 
 workbox.clientsClaim();
